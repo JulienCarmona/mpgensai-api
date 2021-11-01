6ec39306e456e39e353df53928b10011c5dfc5a4
Julien Carmona <julien.carmona44@gmail.com>
2021-11-02 00:04:04 +0100

    Joueur Repository

---
+++ src/main/java/fr/mpgensai/api/persistence/dao/IJoueurRepository.java
@@ -0,0 +1,7 @@
+package fr.mpgensai.api.persistence.dao;
+
+import fr.mpgensai.api.persistence.model.Joueur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IJoueurRepository extends JpaRepository<Joueur, Long> {
+}
--- src/main/java/fr/mpgensai/api/spring/ApiPersistenceJpaConfig.java
+++ src/main/java/fr/mpgensai/api/spring/ApiPersistenceJpaConfig.java
@@ -3,9 +3,11 @@ package fr.mpgensai.api.spring;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @PropertySource({ "classpath:persistence-${persistenceTarget:local}.properties" })
+@EnableJpaRepositories(basePackages = "fr.mpgensai.api.persistence.dao")
 public class ApiPersistenceJpaConfig {
 
     public ApiPersistenceJpaConfig() {
