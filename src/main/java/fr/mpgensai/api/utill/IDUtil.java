3ef632b0e4c5a7a09cb57b491726e67c3d19be06
Julien Carmona <julien.carmona44@gmail.com>
2021-11-02 00:07:35 +0100

    Abstraction for testing Services

---
+++ src/main/java/fr/mpgensai/api/utill/IDUtil.java
@@ -0,0 +1,13 @@
+package fr.mpgensai.api.utill;
+
+import java.util.Random;
+
+public final class IDUtil {
+
+    public final static long randomPositiveLong() {
+        long id = new Random().nextLong() * 10000;
+        id = (id < 0) ? (-1 * id) : id;
+        return id;
+    }
+
+}
