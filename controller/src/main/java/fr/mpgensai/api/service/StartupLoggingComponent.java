1db8823b4520aec3a193453940b30911586ffa7f
Julien Carmona <julien.carmona44@gmail.com>
2021-11-10 18:14:54 -0500

    Corr Slf4j for StartupLoggingComponent

--- controller/src/main/java/fr/mpgensai/api/service/StartupLoggingComponent.java
+++ controller/src/main/java/fr/mpgensai/api/service/StartupLoggingComponent.java
@@ -1,7 +1,6 @@
 package fr.mpgensai.api.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
